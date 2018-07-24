a9463a66e85df0a7a3953a2d03b20fb289936be9
Chieh-jay.liu <b9515038@gmail.com>
2018-07-24 12:25:22 +0800

    [ACC] get phy addr and size from driver

--- src/acc.c
+++ src/acc.c
@@ -4,7 +4,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 struct acc_config
 {
     int fd;
@@ -12,6 +12,9 @@ struct acc_config
     unsigned long dz;
     void *reg;
     unsigned long rz;
+    int rz_fd;
+    unsigned long reg_phy_addr;
+    unsigned long weight_phy_addr;
 };
 
 struct backend
@@ -22,16 +25,54 @@ struct backend
 
 struct backend yolo_backend;
 
+#define ATTRIBUTE_MAX_SIZE (4096)
+
+#define GET_ATTRIBUTE(path, attr, max_size) \
+    do{ \
+        int ret; \
+        int fd; \
+        void *buf; \
+        buf = malloc(max_size); \
+        fd = ret = open(path, O_RDONLY); \
+        if (ret < 0) \
+            goto ERROR_HANDLE; \
+        ret = read(fd, buf, max_size); \
+        if (ret < 0) \
+            goto ERROR_HANDLE; \
+        attr = strtoul(buf, NULL, 0); \
+        close(fd); \
+        free(buf); \
+    }while(0)
 
 void acc_init(struct acc_config *acc)
 {
-    /*FIXME*/
-    acc->rz = 0x1000;
-    acc->dz = 0x1000;
-    /*get mapping*/
-    acc->fd = open("/dev/uio15", O_RDWR);
+    void *buf;
+    int ret;
+   
+ 
+    GET_ATTRIBUTE("/sys/class/uio/uio15/maps/map0/size", acc->rz, ATTRIBUTE_MAX_SIZE);
+    GET_ATTRIBUTE("/sys/class/uio/uio15/maps/map1/size", acc->dz, ATTRIBUTE_MAX_SIZE);
+    GET_ATTRIBUTE("/sys/class/uio/uio15/maps/map0/addr", acc->reg_phy_addr, ATTRIBUTE_MAX_SIZE);
+    GET_ATTRIBUTE("/sys/class/uio/uio15/maps/map1/addr", acc->weight_phy_addr, ATTRIBUTE_MAX_SIZE);
+
+
+    printf("rs = 0x%x, phy addr = 0x%x\n", acc->rz, acc->reg_phy_addr);
+    printf("ws = 0x%x, phy addr = 0x%x\n", acc->dz, acc->weight_phy_addr);
+    
+    ret = acc->fd = open("/dev/uio15", O_RDWR);
     acc->reg = mmap(NULL, acc->rz, PROT_READ | PROT_WRITE, MAP_SHARED, acc->fd, 0); 
     acc->data = mmap(NULL, acc->dz, PROT_READ | PROT_WRITE, MAP_SHARED, acc->fd, 1 * getpagesize()); 
+    
+    return;
+
+ERROR_HANDLE:
+
+    if(ret < 0)
+    {
+        perror("the following error occur");
+        exit(-1);
+    }
+
 }
 
 void validate_acc()
@@ -50,6 +91,6 @@ void validate_acc()
 void main()
 {
     int i;
-    for(i = 0 ; i< 10; i++)
+    for(i = 0 ; i< 1; i++)
         validate_acc();
 }
